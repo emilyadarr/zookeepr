116be30d8a5ad0f9e6df7d186e80dfbb43379915
Emily <emilyadarr@gmail.com>
2022-02-07 15:24:23 -0500

    get animal by id

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { animals } = require('./data/animals.json');
+const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -43,6 +43,11 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
+
 // get method requires two arguments The first is a string that describes the route the client will have to fetch from. The second is a callback function that will execute every time that route is accessed with a GET request.
 app.get('/api/animals', (req, res) => {
   // res parameter (short for response)
@@ -52,6 +57,16 @@ app.get('/api/animals', (req, res) => {
     results = filterByQuery(req.query, results);
   }
   res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  }
+  else {
+    res.send(404);
+  }
 });
 
 app.listen(PORT, () => {
